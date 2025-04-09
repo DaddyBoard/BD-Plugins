c3a2e6a9054d9d0e7d78e79ac5dc24ab673ab4e3
DaddyBoard <48053193+DaddyBoard@users.noreply.github.com>
2025-04-09 01:57:56 +0100

    PML 1.2.0
    New "channel peek" functionality

--- PeekMessageLinks/PeekMessageLinks.plugin.js
+++ PeekMessageLinks/PeekMessageLinks.plugin.js
@@ -1,7 +1,7 @@
 /**
 * @name PeekMessageLinks
 * @author DaddyBoard
-* @version 1.1.2
+* @version 1.2.0
 * @description Clicking on message links will open a popup with the message content.
 * @source https://github.com/DaddyBoard/BD-Plugins
 * @invite ggNWGDV7e2
@@ -16,6 +16,7 @@ const MessageConstructor = Webpack.getByPrototypeKeys("addReaction");
 const UserStore = Webpack.getStore("UserStore");
 const Dispatcher = Webpack.getByKeys("subscribe", "dispatch");
 const ReferencedMessageStore = Webpack.getStore("ReferencedMessageStore");
+const ChannelActions = Webpack.getModule(a => a.clearChannel);
 const updateMessageReferenceStore = (()=>{
     function getActionHandler(){
         const nodes = Dispatcher._actionHandlers._dependencyGraph.nodes;
@@ -30,7 +31,14 @@ const loadThread = BdApi.Webpack.getModule(a => a.loadThread).loadThread;
 const config = {
     changelog: [
         {
-            "title": "1.1.2",
+            "title": "Added",
+            "type": "added",
+            "items": [
+                "Added support for showing the last 50 messages in a channel via the popup when the message link is to a channel, rather than a specific message.",
+            ]
+        },
+        {
+            "title": "Fixes",
             "type": "fixed",
             "items": [
                 "Reverted to the previous caching method (internal memory cache). There was various placement issues with the store method.",
@@ -147,7 +155,6 @@ module.exports = class PeekMessageLinks {
     patchChannelMention() {
         const ChannelMentionBubble = Webpack.getModule(m => m.defaultRules && m.parse).defaultRules.channelMention;       
         Patcher.after("PeekMessageLinks-ChannelMentionBubble", ChannelMentionBubble, "react", (_, [props], res) => {
-            if (!props.messageId) return;
             if (props.content[0].channelType == "10000") {
                 loadThread(props.channelId);
             }
@@ -183,6 +190,10 @@ module.exports = class PeekMessageLinks {
 
                 const targetElement = e.currentTarget;
                 
+                if (targetElement.closest('.peek-message-popup')) {
+                    return;
+                }
+                
                 if (this.popupCloseTimeout) {
                     clearTimeout(this.popupCloseTimeout);
                     this.popupCloseTimeout = null;
@@ -200,6 +211,10 @@ module.exports = class PeekMessageLinks {
                     this.hoverPopupTimeout = null;
                 }
                 
+                if (e.currentTarget.closest('.peek-message-popup')) {
+                    return;
+                }
+                
                 if (this.settings.onHover === "popup") {
                     this.popupCloseTimeout = setTimeout(() => {
                         this.removeAllPopups();
@@ -213,49 +228,64 @@ module.exports = class PeekMessageLinks {
     async handleAction(action, props, targetElement, originalClick, event) {
         if (action === "none") return;
                 
-        let message = MessageStore.getMessage(props.channelId, props.messageId);
+        let messages = [];
 
-        if (!message) {
-            try {
-                const messagePromise = this.messageCache.get(props.messageId) || MessageActions.fetchMessage({
-                    channelId: props.channelId,
-                    messageId: props.messageId
-                });
-                this.messageCache.set(props.messageId, messagePromise);
-                message = await messagePromise;
-
-                if (message.id !== props.messageId) {
-                    message = new MessageConstructor({
-                        id: props.messageId,
-                        flags: 64,
-                        content: "This message has likely been deleted as the returned message ID does not match that of the message link. If you attempt to navigate to the message, you will be redirected to the closest message.",
-                        channel_id: props.channelId,
-                        author: UserStore.getCurrentUser(),
-                    });
-                }
-            } catch (error) {
-                if (originalClick) originalClick(event);
-                return;
+        if (!props.messageId) {
+            let messageStore = Webpack.Stores.MessageStore.getMessages(props.channelId);
+            if (messageStore.ready === false) {
+                await ChannelActions.fetchMessages({channelId:props.channelId});
+                messageStore = Webpack.Stores.MessageStore.getMessages(props.channelId);
             }
-        }
-        if (message.messageReference) {
-            if (ReferencedMessageStore.getMessageByReference(message.messageReference).state !== 0) {
-                let referencedMessage = MessageStore.getMessage(message.messageReference.channel_id, message.messageReference.message_id);
-
-                if (!referencedMessage) {
-
-                    referencedMessage = await MessageActions.fetchMessage({
-                        channelId: message.messageReference.channel_id,
-                        messageId: message.messageReference.message_id
-                    });
-                }
-                updateMessageReferenceStore(referencedMessage);
+            if (messageStore && messageStore._array) {
+                messages = messageStore._array.slice(-50);
             }
+        } else {
+            let message = MessageStore.getMessage(props.channelId, props.messageId);
+
+            if (!message) {
+                try {
+                    const messagePromise = this.messageCache.get(props.messageId) || MessageActions.fetchMessage({
+                        channelId: props.channelId,
+                        messageId: props.messageId
+                    });
+                    this.messageCache.set(props.messageId, messagePromise);
+                    message = await messagePromise;
+
+                    if (message.id !== props.messageId) {
+                        message = new MessageConstructor({
+                            id: props.messageId,
+                            flags: 64,
+                            content: "This message has likely been deleted as the returned message ID does not match that of the message link. If you attempt to navigate to the message, you will be redirected to the closest message.",
+                            channel_id: props.channelId,
+                            author: UserStore.getCurrentUser(),
+                        });
+                    }
+                } catch (error) {
+                    if (originalClick) originalClick(event);
+                    return;
+                }
+            }
+            
+            if (message.messageReference) {
+                if (ReferencedMessageStore.getMessageByReference(message.messageReference).state !== 0) {
+                    let referencedMessage = MessageStore.getMessage(message.messageReference.channel_id, message.messageReference.message_id);
+
+                    if (!referencedMessage) {
+                        referencedMessage = await MessageActions.fetchMessage({
+                            channelId: message.messageReference.channel_id,
+                            messageId: message.messageReference.message_id
+                        });
+                    }
+                    updateMessageReferenceStore(referencedMessage);
+                }
+            }
+            
+            messages = [message];
         }
 
-        if (action === "popup") {
+        if (action === "popup" && messages.length > 0) {
             const rect = targetElement.getBoundingClientRect();
-            const popup = this.showMessagePopup(message, rect);
+            const popup = this.showMessagePopup(messages, rect, props.channelId);
             
             const closePopup = (e) => {
                 if (popup && document.body.contains(popup) && !popup.contains(e.target)) {
@@ -271,8 +301,8 @@ module.exports = class PeekMessageLinks {
         }
     }
 
-    showMessagePopup(message, targetRect) {
-        if (!message || !message.author) return;
+    showMessagePopup(messages, targetRect, channelId) {
+        if (!messages || messages.length === 0) return;
         
         this.removeAllPopups();
         
@@ -303,10 +333,15 @@ module.exports = class PeekMessageLinks {
             .peek-message-popup [class*="message__"]:hover:not([class*="mentioned__"]) {
                 background: inherit !important;
             }
+            
+            .peek-message-popup .message-item {
+                margin-top: 8px;
+                padding-top: 8px;
+            }
+
         `);
 
-        const channel = ChannelStore.getChannel(message.channel_id);
-        console.log(message);
+        const channel = ChannelStore.getChannel(channelId || (messages[0] && messages[0].channel_id));
         
         if (!channel) {
             console.log("No channel found");
@@ -314,24 +349,52 @@ module.exports = class PeekMessageLinks {
         }
 
         const PopupComponent = () => {
-            const maxHeight = 300;
-            const buffer = 40;
+            const maxHeight = 500;
+            const buffer = 20;
+            const edgeMargin = 40;
+            
             const spaceAbove = targetRect.top;
-            const showBelow = spaceAbove < (maxHeight + buffer);
+            const spaceBelow = window.innerHeight - targetRect.bottom;
+            
+            const showBelow = spaceAbove < spaceBelow || spaceAbove < 200;
+            
+            const adjustedMaxHeight = Math.min(
+                maxHeight,
+                showBelow ? spaceBelow - buffer - edgeMargin : spaceAbove - buffer - edgeMargin
+            );
+            
+            let topPosition = targetRect.bottom + 10;
+            if (topPosition + adjustedMaxHeight > window.innerHeight - edgeMargin) {
+                topPosition = window.innerHeight - adjustedMaxHeight - edgeMargin;
+            }
+            
+            let bottomPosition = window.innerHeight - targetRect.top + 10;
+            if (window.innerHeight - bottomPosition - adjustedMaxHeight < edgeMargin) {
+                bottomPosition = window.innerHeight - adjustedMaxHeight - edgeMargin;
+            }
+            
+            const containerRef = React.useRef(null);
+            
+            React.useEffect(() => {
+                if (containerRef.current) {
+                    containerRef.current.scrollTop = containerRef.current.scrollHeight;
+                }
+            }, []);
 
             return React.createElement('div', {
+                ref: containerRef,
                 style: {
                     position: 'fixed',
                     ...(showBelow
-                        ? { top: `${targetRect.bottom + 10}px` }
-                        : { bottom: `${window.innerHeight - targetRect.top + 10}px` }
+                        ? { top: `${topPosition}px` }
+                        : { bottom: `${bottomPosition}px` }
                     ),
-                    left: `${targetRect.left}px`,
+                    left: `${Math.min(Math.max(edgeMargin, targetRect.left), window.innerWidth - 460 - edgeMargin)}px`,
                     backgroundColor: 'var(--background-primary)',
                     borderRadius: '8px',
                     padding: '8px',
                     width: '460px',
-                    maxHeight: '300px',
+                    maxHeight: `${adjustedMaxHeight}px`,
                     overflowY: 'scroll',
                     boxShadow: 'var(--elevation-high)',
                     zIndex: 1001,
@@ -351,23 +414,29 @@ module.exports = class PeekMessageLinks {
                         padding: 0
                     }
                 }, 
-                    React.createElement(Message, {
-                        id: `${message.id}-${message.id}`,
-                        groupId: message.id,
-                        channel: channel,
-                        message: message,
-                        compact: false,
-                        isLastItem: true,
-                        renderContentOnly: false,
-                        style: {
-                            width: '100%'
-                        }
+                    messages.map((message, index) => {
+                        return React.createElement('div', {
+                            className: 'message-item',
+                            key: `${message.id}-container-${index}`
+                        }, React.createElement(Message, {
+                            id: `${message.id}-${index}`,
+                            groupId: message.id,
+                            channel: channel,
+                            message: message,
+                            compact: false,
+                            isLastItem: index === messages.length - 1,
+                            renderContentOnly: false,
+                            style: {
+                                width: '100%'
+                            }
+                        }));
                     })
                 )
             );
         };
 
         ReactDOM.render(React.createElement(PopupComponent), popupElement);
+        
         return popupElement;
     }
 
